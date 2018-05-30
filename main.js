292fe41adadf4d5b7e64396ac6917ffe2e4ef853
desi-digitalus <32277869+desi-digitalus@users.noreply.github.com>
2018-05-30 13:50:32 -0400

    Delete main.js
    delet main js

--- main.js
+++
@@ -1,9 +0,0 @@
-// JavaScript Document
-
-$( document ).ready(function() {
-	$('#headerVideoLink').magnificPopup({
-	type:'inline',
-	midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
-	});
-});
-   
