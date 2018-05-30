1710501d7bb6f11f753fc4f7b5876a1e0e1037a9
desi-digitalus <32277869+desi-digitalus@users.noreply.github.com>
2018-05-30 12:38:26 -0400

    Add files via upload
    project files-index.html, main.js, style.css

---
+++ main.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+
+$( document ).ready(function() {
+	$('#headerVideoLink').magnificPopup({
+	type:'inline',
+	midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+	});
+});
+   
